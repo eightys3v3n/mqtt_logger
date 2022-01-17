7ea4795238d0605f58776c69bea67ec6af8d780a
eightys3v3n <eightys3v3n@gmail.com>
2022-01-16 18:45:35 -0700

    added last_updated to hosts

--- sql_templates.py
+++ sql_templates.py
@@ -1,9 +1,13 @@
+""" SQL for tables that are used in more than one module."""
+
+
 Hosts = """CREATE TABLE IF NOT EXISTS
 hosts(
-    name        VARCHAR(128) PRIMARY KEY,
-    IP          VARCHAR(15),
-    description VARCHAR(256),
-    SSID        VARCHAR(64),
-    MAC         VARCHAR(17),
-    RSSI        INT)
+    last_updated DATETIME NOT NULL,
+    name         VARCHAR(128) PRIMARY KEY,
+    IP           VARCHAR(15),
+    description  VARCHAR(256),
+    SSID         VARCHAR(64),
+    MAC          VARCHAR(17),
+    RSSI         INT)
 """
